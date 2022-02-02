026689c4d24f5f65b442267f80089983720621f5
ksoon1985 <ksoon1985@naver.com>
2022-02-02 23:12:48 +0900

    초기설정

---
+++ src/main/java/com/ksh/board/SpringbootBoardBasicApplication.java
@@ -0,0 +1,13 @@
+package com.ksh.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBoardBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBoardBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksh/board/SpringbootBoardBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksh.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoardBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
